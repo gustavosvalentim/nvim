0875a27cab7f143603d456d00224b33ee59c5c83
Gustavo Valentim <gustavosvalentim1@gmail.com>
2025-10-13 23:01:56 -0300

    feat: editor settings for html and django

--- after/plugin/fzf.lua
+++ after/plugin/fzf.lua
@@ -3,5 +3,5 @@ local fzf = require('fzf-lua')
 fzf.setup({ 'fzf-native' })
 
 vim.keymap.set('n', '<leader>p', fzf.files)
-vim.keymap.set('n', '<leader>ff', fzf.lgrep_curbuf)
-vim.keymap.set('n', '<leader>fp', fzf.live_grep)
+vim.keymap.set('n', '<leader>f', fzf.lgrep_curbuf)
+vim.keymap.set('n', '<leader>F', fzf.live_grep)
