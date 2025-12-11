e07e3cd7fbc0bbb551b539d9fff17e50c08fd26b
Gustavo Valentim <gustavosvalentim1@gmail.com>
2025-12-11 11:19:10 -0300

    feat: keymap to go to tab by number

--- lua/gsv/tabs.lua
+++ lua/gsv/tabs.lua
@@ -1,2 +1,6 @@
 vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<cr>", { desc = "Create new tab" })
-vim.keymap.set("n", "<Leader>tc", "<cmd>tabclose<cr>", { desc = "Close current tab" })
+vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<cr>", { desc = "Close current tab" })
+
+for i = 1, 9, 1 do
+  vim.keymap.set("n", "<leader>" .. i, i .. "gt", { desc = "Go to tab " .. i })
+end
